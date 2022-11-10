41ac68b486f4b43c43a98aff6d3e2dc39b83abf2
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2022-11-09 22:26:59 -0300

    Feat: Desafio 15

---
+++ challenges/desafio15.js
@@ -0,0 +1,26 @@
+db.produtos.updateMany(
+  {},
+  { $set: { avaliacao: 0 } },
+);
+
+db.produtos.updateMany(
+  {
+    $and: [
+      { tags: { $exists: true } },
+      { tags: { $elemMatch: { $eq: "bovino" } } },
+    ],
+  },
+  { $set: { avaliacao: 5 } },
+);
+
+db.produtos.updateMany(
+  {
+    $and: [
+      { tags: { $exists: true } },
+      { tags: { $elemMatch: { $eq: "ave" } } },
+    ],
+  },
+  { $set: { avaliacao: 3 } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });
