aad61d2dcb551e454f0c4c0392a0254b143b5bd9
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2022-11-09 22:35:38 -0300

    Fix: Desafio 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,6 +1,13 @@
-db.produtos.find({ 
-  $and: [
-    { nome: { $ne: "Big Mac" } },
-    { nome: { $ne: "McChicken" } },
-  ],
-}, { _id: 0, nome: 1, curtidas: 1, vendidos: 1 });
+db.produtos.find({
+  nome: {
+      $nin: [
+          "Big Mac",
+          "McChicken",
+      ],
+  } },
+  {
+      _id: 0,
+      nome: 1,
+      curtidas: 1,
+      vendidos: 1,
+});
